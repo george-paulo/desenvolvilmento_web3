e189404cbb8750145de804004dc545ffe7f1530c
George Paulo <george.paulo02@aluno.ifce.edu.br>
2024-03-13 20:17:16 -0300

    Autenticação
    Autenticação com sucesso.

---
+++ app/addUser.js
@@ -0,0 +1,42 @@
+const AdminController = require('./controllers/AdminController');
+
+
+function getAdminController() {
+    return new Promise((resolve, reject) => {
+        try {
+            const adminController = new AdminController();
+            resolve(adminController);
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
+
+
+async function addUser(adminController) {
+    
+    const userDetails = {
+        body: {
+            nome: 'George',
+            senha: '123456'
+        }
+    };
+
+    
+    const fakeRes = {
+        redirect: () => {}
+    };
+
+   
+    try {
+        await adminController.create(userDetails, fakeRes);
+        console.log('Usuário criado com sucesso');
+    } catch (error) {
+        console.error('Erro ao criar usuário:', error);
+    }
+}
+
+
+getAdminController().then(adminController => {
+    addUser(adminController);
+});
---
+++ app/controllers/CercaController.js
@@ -0,0 +1,70 @@
+const getCerca = require('../models/Cerca');
+const Cerca = require('../lib/Cerca');
+
+class CercaController {
+    constructor() {
+        this.Cerca = null;
+        getCerca().then((model) => {
+          this.Cerca = model;
+          console.log('Modelo Cerca carregado:', this.Cerca);
+        });
+    }
+
+    async create(req, res) {
+        const { nome, lado } = req.body;
+        const cerca = await this.Cerca.create({ nome, lado });
+
+        
+        const cercaObj = new Cerca(nome, lado);
+        const area = cercaObj.area();
+
+        
+        res.render('resposta', { area });
+    }
+
+    async list(req, res) {
+        console.log('Rota /cercas chamada');
+        const cercas = await this.Cerca.findAll();
+
+        
+        cercas.forEach(cerca => {
+            const cercaObj = new Cerca(cerca.nome, cerca.lado);
+            cerca.area = cercaObj.area();
+        });
+
+        console.log('Cercas recuperadas do banco de dados:', cercas);
+        res.render('cercas', { cercas });
+    }
+
+    async editar(req, res) {
+        const { id } = req.params;
+        const cerca = await this.Cerca.findOne({ where: { id } });
+        if (!cerca) {
+            return res.redirect('/cercas');
+        }
+        res.render('editar', { cerca });
+    }
+
+    async update(req, res) {
+        console.log('Update chamado o metódo update');
+        const { id, nome, lado } = req.body;
+        console.log('Parameters:', id, nome, lado);
+        await this.Cerca.update({ nome, lado }, { where: { id } });
+        res.redirect('/cercas');
+    }
+
+    async delete(req, res) {
+        const { id } = req.params;
+        await this.Cerca.destroy({ where: { id } });
+        req.session.messages = { success: 'Cerca excluída com sucesso.' };
+        res.redirect('/cercas');
+    }
+
+    async getById(req, res) {
+        const { id } = req.params;
+        const cerca = await this.Cerca.findOne({ where: { id } });
+        res.render('cerca', { cerca });
+    }
+}
+
+module.exports = CercaController;
