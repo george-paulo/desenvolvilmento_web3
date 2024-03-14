cb218cc5e0067f2e5d5973f1999ed7afb39fb41a
George Paulo <george.paulo02@aluno.ifce.edu.br>
2024-03-13 21:08:52 -0300

    Autenticao
    Auntenteicao

--- app/app.js
+++ app/app.js
@@ -2,19 +2,43 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const router = require('./routes/index');
+const session = require('express-session');
+const flash = require('connect-flash');
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+
+require('dotenv').config();
+
+app.use(methodOverride('_method'));
+app.use(session({
+    secret: 'sua chave secreta',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 }
+}));
+
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.messages = req.session.messages;
+    delete req.session.messages;
+    next();
+});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 app.use((req, res, next) => {
     console.log(`[${new Date().toLocaleString('pt-BR', { timeZone: 'America/Sao_Paulo' })}] ${req.method} to ${req.url}`);
     next();
 });
-
 app.use('/', router);
-
 app.use((req, res, next) => {
     res.status(404).send('Página não encontrada');
 });
-
 app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,33 @@
-const SobreController = require('../controllers/SobreController');
 const express = require('express');
 const router = express.Router();
 const Cerca = require('../lib/Cerca');
-//consertado
-const sobreController = new SobreController();
+const CercaController = require('../controllers/CercaController');
+const AdminController = require('../controllers/AdminController');
+const SobreController = require('../controllers/SobreController'); 
+const auth = require('../controllers/auth');
 
+const adminController = new AdminController();
+const sobreController = new SobreController(); 
+const cercaController = new CercaController();
+
+// O restante do seu código...
+
+router.get('/cercas/editar/:id', auth, (req, res) => {
+    const { id } = req.params;
+    cercaController.editar(req, res);
+});
+
+router.get('/admin', auth, (req, res) => adminController.list(req, res));
+router.post('/cercas', auth, (req, res) => cercaController.create(req, res));
+router.get('/cercas', auth, (req, res) => cercaController.list(req, res));
+router.get('/admin/create', auth, (req, res) => res.render('criar_admin'));
+router.post('/admin/create', auth, (req, res) => adminController.create(req, res));
+router.get('/admin/login', (req, res) => adminController.showLoginForm(req, res));
+router.post('/admin/login', (req, res) => adminController.login(req, res));
+router.get('/admins', auth, (req, res) => adminController.list(req, res));
+router.put('/cercas/:id', auth, (req, res) => cercaController.update(req, res));
+router.delete('/cercas/:id', auth, (req, res) => cercaController.delete(req, res));
+router.get('/cercas/:id', auth, (req, res) => cercaController.getById(req, res));
 router.get('/', (req, res) => res.render('index'));
 router.get('/index', (req, res) => res.redirect('/'));
 router.get('/sobre', (req, res) => sobreController.index(req, res));
@@ -13,5 +36,4 @@ router.get('/resposta', (req, res) => {
     const cerca = new Cerca(nome, lado, id);
     res.render('resposta', { area: cerca.area() });
 });
-
 module.exports = router;
