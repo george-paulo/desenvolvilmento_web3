e86a7c36f8674afd5df7e55042b182eb30826d5c
George Paulo <122838710+george-paulo@users.noreply.github.com>
2024-03-13 23:47:56 -0300

    Criação de usuário
    Para poder criar o usuário, tirar todos os "auth,". Depois de criado, recolocar para garantir a segurança.

--- app/routes/index.js
+++ app/routes/index.js
@@ -10,23 +10,31 @@ const adminController = new AdminController();
 const sobreController = new SobreController(); 
 const cercaController = new CercaController();
 
-// O restante do seu código...
-
+//tirar "auth,"
 router.get('/cercas/editar/:id', auth, (req, res) => {
     const { id } = req.params;
     cercaController.editar(req, res);
 });
 
+//tirar "auth,"
 router.get('/admin', auth, (req, res) => adminController.list(req, res));
+//tirar "auth,"
 router.post('/cercas', auth, (req, res) => cercaController.create(req, res));
+//tirar "auth,"
 router.get('/cercas', auth, (req, res) => cercaController.list(req, res));
+//tirar "auth,"
 router.get('/admin/create', auth, (req, res) => res.render('criar_admin'));
+//tirar "auth,"
 router.post('/admin/create', auth, (req, res) => adminController.create(req, res));
 router.get('/admin/login', (req, res) => adminController.showLoginForm(req, res));
 router.post('/admin/login', (req, res) => adminController.login(req, res));
+//tirar "auth,"
 router.get('/admins', auth, (req, res) => adminController.list(req, res));
+//tirar "auth,"
 router.put('/cercas/:id', auth, (req, res) => cercaController.update(req, res));
+//tirar "auth,"
 router.delete('/cercas/:id', auth, (req, res) => cercaController.delete(req, res));
+//tirar "auth,"
 router.get('/cercas/:id', auth, (req, res) => cercaController.getById(req, res));
 router.get('/', (req, res) => res.render('index'));
 router.get('/index', (req, res) => res.redirect('/'));
